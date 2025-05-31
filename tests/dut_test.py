6782daa18a2f84e0c61793144c7eb3ec814c1d80
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-05-31 11:20:10 +0530

    dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -3,20 +3,14 @@ from cocotb.triggers import Timer
 from cocotb.result import TestFailure
 
 @cocotb.test()
-async def dut_test(dut):
-    test vectors = [
-        (0,0,0),
-        (0,1,1),
-        (1,0,1),
-        (1,1,0)
-    ]
-    for a, b, expected in test_vectors:
-        dut.a.value = a 
-        dut.b.value = b
+async def test_xor_basic(dut):
+   
+    test_vectors = [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 0)]
 
-        await Timer( 1, units='ns')
-    
+    for a, b, expected in test_vectors:
+        dut.a.value = a
+        dut.b.value = b
+        await Timer(1, units='ns')
         actual = int(dut.y.value)
-        dut.log.info(f"Testing a = {a}, b={b} => y={actual}")
-    
-        assert actual == expected, f"FAILED": a={a}, b={b}, expected={expected},got={actual}"
+        dut._log.info(f"Inputs: a={a}, b={b} => y={actual}")
+        assert actual == expected, f"FAILED: a={a}, b={b}, expected={expected}, got={actual}"
