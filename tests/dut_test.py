5ab636aae94cc2f36451d0d3a4e85944ed509e27
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-05-31 11:03:14 +0530

    dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,7 +1,22 @@
 import cocotb
 from cocotb.triggers import Timer
-
+from cocotb.result import TestFailure
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 0, "Test Not Implemented Error"
+    test vector = [
+        (0,0,0),
+        (0,1,1),
+        (1,0,1),
+        (1,1,0)
+    ]
+    for a, b, expected in test_vectors:
+        dut.a.value = a 
+        dut.b.value = b
+
+    await Timer( 1, units='ns')
+    
+    actual = int(dut.y.value)
+    dut.log.info(f"Testing a = {a}, b={b} => y={actual}")
+    
+    assert actual == expected, f"FAILED": a={a}, b={b}, expected={expected},got={actual}"
